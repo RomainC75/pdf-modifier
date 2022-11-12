f98660489621bab21cd4ebc750582e376b792535
RomainC75 <rom.chenard@gmail.com>
2022-11-12 15:09:51 +0100

    errors : 93/200

--- utils/extract_secu.py
+++ utils/extract_secu.py
@@ -6,7 +6,10 @@ import os
 from pdf2image import convert_from_path
 from dotenv import dotenv_values
 import json
-import pandas
+import pandas as pd
+import math
+from matplotlib import pyplot as plt
+from unidecode import unidecode
 
 config = dotenv_values(".env.folders")
 
@@ -16,8 +19,8 @@ TEMP_FOLDER = config["TEMP_FOLDER"]
 
 
 
-def convert_first_page_to_image(path_to_empty_pdf):
-    doc = convert_from_path(path_to_empty_pdf)
+def convert_first_page_to_image_and_save_to_tempFile(path_to_empty_pdf):
+    doc = convert_from_path(path_to_empty_pdf, dpi=400)
     # path, fileName = os.path.split(path_to_empty_pdf)
     # fileBaseName, fileExtension = os.path.splitext(fileName)
     #write down first page
@@ -26,60 +29,78 @@ def convert_first_page_to_image(path_to_empty_pdf):
 
 
 def select_rectangle_and_change_colors(img):
-    cv2.imshow('img', img)
+    # cv2.imshow('img', img)
     # startP=(230,670)
     # endP=(755,730)
     
-    startP=(400,1870)
-    endP=(945,1915)
-    secu_rect = img[startP[1]:endP[1], startP[0]:endP[0] ]
+    startP=(780,3750)
+    endP=(1800,3900)
+    img_raw = img[startP[1]:endP[1], startP[0]:endP[0] ]
     # print(filter(lambda triad:triad[0][0]!=255, secu_rect))
-    yellow_bottom=[]
-    yellow_top=[]
+    
+    img_hsv = cv2.cvtColor(img_raw, cv2.COLOR_BGR2HSV)
+    img_changing = cv2.cvtColor(img_raw, cv2.COLOR_RGB2GRAY)
+    low_color = np.array([0, 0, 0])
+    high_color = np.array([180, 255, 30])
+    blackColorMask = cv2.inRange(img_hsv, low_color, high_color)
+    img_black_filtered = cv2.bitwise_and(img_changing, img_changing, mask = blackColorMask)
+    
+    # cv2.imshow("raw", img_raw)
+    # cv2.imshow("mask", blackColorMask)
+    # cv2.imshow("changing :", img_changing)
+    # cv2.imshow("filterd : ", img_black_filtered)
+
+    img_copy=img_raw.copy()
+    img_canny = cv2.Canny(img_copy, 180, 200, apertureSize=3)
+
+    # img_hough = cv2.HoughLinesP(img_canny, 1, math.pi / 180, 100, minLineLength = 100, maxLineGap = 10)
+    # cv2.imshow("hough : ", img_hough)
+    # (x, y, w, h) = (np.amin(img_hough, axis = 0)[0,0], np.amin(img_hough, axis = 0)[0,1], np.amax(img_hough, axis = 0)[0,0] - np.amin(img_hough, axis = 0)[0,0], np.amax(img_hough, axis = 0)[0,1] - np.amin(img_hough, axis = 0)[0,1])
+    # img_roi = img_copy[y:y+h,x:x+w]
 
     # secu_morphed = cv2.cvtColor(secu_rect, cv2.COLOR_BGR2GRAY)
     # secu_morphed = cv2.GaussianBlur(secu_morphed,(5,5),0)
     # secu_morphed = cv2.Canny(secu_morphed,75,100)
 
-    secu_morphed = cv2.cvtColor(secu_rect, cv2.COLOR_BGR2BGRA)
-    # secu_morphed = cv2.Canny(secu_morphed,75,100)
-    # low_color = np.array([0, 0, 0])
-    # high_color = np.array([180, 179, 50])
-    # secu_morphed = cv2.inRange(secu_rect,low_color, high_color)
-    # secu_morphed = cv2.bitwise_not(secu_morphed)
     
-    cv2.imwrite(TEMP_FOLDER+'selection.jpg', secu_morphed)
+    
+    cv2.imwrite(TEMP_FOLDER+'selection.jpg', img_raw)
 
-    rectangle = [ 
-        [],
-        []
-    ]
 
-    return secu_morphed
+    return img_changing
 
 def extract_secu_as_string(emptypdf_name):
     pdf_path = DOCS_FOLDER+emptypdf_name
     # convert and create "temp.jpg"
-    convert_first_page_to_image(pdf_path)
+    convert_first_page_to_image_and_save_to_tempFile(pdf_path)
     
     img = cv2.imread(TEMP_FOLDER+'temp.jpg')
     secu_morphed = select_rectangle_and_change_colors(img)
 
-    txt = pytesseract.image_to_string(secu_morphed).encode("utf-8")
-    print("raw : ", txt)
-    txt = txt.decode('ascii')
-    secu = re.findall(r"[0-9]",txt)
-    secu_string = "".join(secu)
-    print("n° SECU  : ", secu_string)
+    
 
     #remove
     #just print the layers and data
     data = pytesseract.image_to_data(secu_morphed)
     # convert to pandas
     dataList = list(map(lambda x: x.split('\t'),data.split('\n') ))
-    df = pandas.DataFrame(dataList[1:],columns=dataList[0])
+    df = pd.DataFrame(dataList[1:],columns=dataList[0])
     df.dropna(inplace=True)
     print("===> DATA : ",data)
+
+    
+    # txt = pytesseract.image_to_string(secu_morphed).encode("utf-8")
+    # print("raw : ", txt, len(txt))
+    # txt = txt.decode('ascii')
+    # secu = re.findall(r"[0-9]",txt)
+    # secu_string = "".join(secu)
+    # print("n° SECU  : ", secu_string)
+
+    txt_raw = pytesseract.image_to_string(secu_morphed)
+    txt = unidecode(txt_raw)
+    secu = re.findall(r"[0-9]",txt)
+    secu_string = "".join(secu)
+    print("n° SECU  : ", secu_string, " len : ", len(secu_string))
 
     #remove
     #rectangles
@@ -93,7 +114,7 @@ def extract_secu_as_string(emptypdf_name):
         # Affiche un rectangle
         cv2.rectangle(secu_morphed, (x, y), (x + w, y + h), (0, 255, 0), 2)
     
-    cv2.imshow('img', secu_morphed)
+    # cv2.imshow('img', secu_morphed)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
